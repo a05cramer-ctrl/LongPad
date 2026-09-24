67f9a0107e0c8d4c770e32dd9be1e64349a17991
a05cramer-ctrl <a05cramer@gmail.com>
2026-09-24 16:51:04 +0200

    Add CA and X link

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.PERPER_CFG = {
-  CA: "",
+  CA: "Ckn4ayTZZwto3GJD1yK816gwZt7krVoT3V2wEd8Bpump",
   PAD: "pumpfun",
-  X: "https://x.com/",
-  BUY: "",
-  CHART: ""
+  X: "https://x.com/useperper",
+  BUY: "https://pump.fun/coin/Ckn4ayTZZwto3GJD1yK816gwZt7krVoT3V2wEd8Bpump",
+  CHART: "https://gmgn.ai/sol/token/Ckn4ayTZZwto3GJD1yK816gwZt7krVoT3V2wEd8Bpump"
 };
